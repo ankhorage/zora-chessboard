18218c60f01a67abf87fc20499420ac0f5cf8572
artiphishle <fabio.gartenmann@protonmail.com>
2026-05-18 23:45:07 +0200

    chore: fix docs

--- paradox.config.ts
+++ paradox.config.ts
@@ -4,9 +4,8 @@ export default defineParadoxConfig({
   mode: 'write',
 
   docs: {
-    title: '@ankhorage/zora',
-    description:
-      'ZORA — an opinionated UI kit for React Native + Web built on top of @ankhorage/surface.',
+    title: '@ankhorage/zora-chessboard',
+    description: 'Chessboard component for React Native and React Native Web apps built on ZORA.',
   },
 
   package: {
