ac6947e27c835ca4907884be58c98c28aedc0a40
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-19 00:40:06 +0200

    feat: expose chess component exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-/* todo */
+export { ChessBoard } from './ChessBoard';
+export { createChessBoardColorScheme } from './colors';
+export type { ChessColorThemeShape } from './colors';
+export { getLegalTargets, readChessPieces, tryMove } from './engine';
+export type { ChessPieceState } from './engine';
+export { chessBoardMeta } from './meta';
+export { ZORA_CHESS_COMPONENT_META } from './registry';
+export { chessSquares, createBoardSquares, getSquareFile, getSquareRank, isLightSquare } from './squares';
+export type {
+  ChessBoardColorOverrides,
+  ChessBoardColorScheme,
+  ChessBoardOrientation,
+  ChessBoardProps,
+  ChessMoveAttempt,
+  ChessMoveResult,
+  ChessPieceCode,
+  ChessPieceRenderer,
+  ChessPieceRenderContext,
+  ChessPromotionPiece,
+  ChessSquareId,
+} from './types';
